b6a2a9b6232027ad556b60f929310ce7fe9745e7
Ayaz Ahemd <forgithub44@gmail.com>
2024-05-22 23:26:56 +0400

    Project building first step

---
+++ script.js
@@ -0,0 +1,146 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+/*
+let arr = [0, 1, 2, 3, 4];
+let arr2 = [1, 2, 3, 4, 5];
+
+console.log(arr.slice(3));
+let allArray = [...arr, ...arr2];
+console.log(allArray);
+console.log(
+  allArray.reduce(e => {
+    console.log(e);
+  })
+);
+
+console.log(arr.reverse());
+console.log(arr.concat(allArray));
+console.log(arr);
+arr.push(...allArray);
+console.log(arr);
+// console.log(arr.splice(2, 5));
+
+console.log(arr.at(-1));
+*/
+/*
+
+const mmovements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (let [ind, mov] of mmovements.entries()) {
+  if (mov > 0) {
+    console.log(`Movement ${ind + 1}: You deposit ${mov}`);
+  } else {
+    console.log(`Movement ${ind + 1}: You withdraw ${mov}`);
+  }
+}
+
+console.log('-----FOR EACH LOOP using method');
+
+mmovements.forEach((val, ind, arr) => {
+  console.log(val, ind, arr[ind]);
+});
+*/
+/*
+const dummyAccount = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+dummyAccount.movements.forEach((val, ind, arr) => {
+  console.log(val, ind, arr[++ind]);
+  console.log(arr.at(5));
+});
+
+*/
+
+const currencian = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencian.forEach((val, ind, map) => {
+  console.log(ind, val);
+});
+
+let currencianUnique = new Set(['usd', 'gbp', 'usd', ' usd', 'pkr']);
+console.log(currencianUnique);
+currencianUnique.forEach((value, index, set) => {
+  console.log(index, value);
+});
